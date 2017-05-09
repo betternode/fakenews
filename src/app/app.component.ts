5891fcb84aedf7375854c2062626e53cc828a413
Kushal Hada (developer) <kushaldeveloper@gmail.com>
2017-05-09 15:09:53 -0400

    add a contact form
    Signed-off-by: Kushal Hada (developer) <kushaldeveloper@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'RWFN';
+  motivationalFirstParagraph = "This website will be the home for fake news. If you would like to keep in touch, please fill out our contact form.";
+  linkToContactForm = "https://goo.gl/forms/oXjsRXBx8MrgFHc83";
 }
